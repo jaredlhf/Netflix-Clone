915facd82899d946ab1bf2e566552c937007ab54
jaredlhf <jlhfqaz@gmail.com>
2022-02-10 15:30:07 +0800

    fix heading typo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="app">
       <Nav/>
       <Banner/>
-      <Row title="NETFLIX_ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
